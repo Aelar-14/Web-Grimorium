349eb9062755b042dc2dd6576c1def4f294b6b7b
Aelar 14 <85886928+Aelar-14@users.noreply.github.com>
2021-09-20 20:00:34 -0300

    Update iro.js
    nova mudança

--- iro.js
+++ iro.js
@@ -1,68 +1,63 @@
-function iro(){
+
+
+function teclaDw(){
 var diry, dirx, jog, pjx, pjy, vel;
 var tamTelaW, tamTelaH;
 var jogo;
 var frames;
-
-
-function teclaDw(){
-
-var tecla = eventKeyCode;
-if (tecla == w){ // é cima, direcional superior
-		diry = -1;;
+function teclaDown(){
+var tecla=event.KeyCode;
+if (tecla==87){ // é cima, direcional superior
+		diry=-1;;
+ }else if (tecla==83){ // direcional inferior
+		diry=+1;
  }
-		else if (tecla == s){ // direcional inferior
-		diry = +1;
- }
-if (tecla == a) {
-		dirx = -1;
- }
-		else if (tecla == d){
-		dirx = +1;
+if (tecla==65) {
+		dirx=-1;
+ }else if (tecla==68){
+		dirx=+1;
  }
 function teclaUp(){
-		var tecla = event.KeyCode;
-		if ((tecla == w)||(tecla == s)){
-		diry = 0;
+		var tecla=event.KeyCode;
+		if ((tecla==87)||(tecla==83)){
+		diry=0;
  }
-		if ((tecla == a)||(tecla == d)){
-		dirx = 0;
+		if ((tecla==65)||(tecla==68)){
+		dirx=0;
  }
 }
 
+document.addEventListener("keydown", teclaDown);
+document.addEventListener("keyup", teclaUp);
+
 function move(){
       console.log();
-		pjy += diry*vel;
-		pjx += dirx*vel;
-		jog.style.top = pjy + "px";
-    	jog.style.left = pjx + "px";
+		pjy+=diry*vel;
+		pjx+=dirx*vel;
+		jog.style.top=pjy+"px";
+    	jog.style.left=pjx+"px";
 }
 
 function game(){
       console.log();
-		if(jogo === true){
+		if(jogo=true){
 		move();
  }
  var frames = requestAnimationFrame(game);
 }
 
 function inicia(){
-console.log();
- var jogo =true;
- tamTelaH = window.innerHeight;
- tamTelaW = window.innerWidth;
+ var jogo=true;
+ tamTelaH=window.innerHeight;
+ tamTelaW=window.innerWidth;
 
- dirx = 0;
- diry = 0;
- pjx = tamTelaW/2;
- pjy = tamTelaH/2;
- vel = 5;
- jog = document.getElementById("pers1");
- jog.style.top = pjy + "px";
- jog.style.left = pjx + "px";
+ dirx=0;
+ diry=0;
+ pjx=tamTelaW/2;
+ pjy=tamTelaH/2;
+ vel=5;
+ jog=document.getElementById('aramisJog').appendChild(img);
+ jog.style.top=pjy+"px";
+ jog.style.left=pjx+"px";
  game();
 }
- console.log();
- window.addEventListener("load", inicia);
- document.addEventListener("keydown", teclaDw);
- document.addEventListener("keyup", teclaUp);
