af7e8d499718e0bdc6502bfa9c599d8fad1f8ebc
Aelar 14 <85886928+Aelar-14@users.noreply.github.com>
2021-09-21 06:13:03 -0300

    Update iro.js
    uma update muito importante com ajuda de Douglas Varollo, da rocketseat
    vlw ae bro
    :3

--- iro.js
+++ iro.js
@@ -1,65 +1,41 @@
+const player = {
+  coordinates: {
+    x: 0,
+    y: 0
+  },
+  element: document.getElementById('pers1')
+};
 
+function movePlayer(player) {
+  const { x, y } = player.coordinates;
 
-function teclaDw(){
-var diry, dirx, jog, pjx, pjy, vel;
-var tamTelaW, tamTelaH;
-var jogo;
-var frames;
-function teclaDown(){
-var tecla=event.KeyCode;
-if (tecla==87){ // é cima, direcional superior
-		diry=-1;;
- }else if (tecla==83){ // direcional inferior
-		diry=+1;
- }
-if (tecla==65) {
-		dirx=-1;
- }else if (tecla==68){
-		dirx=+1;
- }
-}
-function teclaUp(){
-		var tecla=event.KeyCode;
-		if ((tecla==87)||(tecla==83)){
-		diry=0;
- }
-		if ((tecla==65)||(tecla==68)){
-		dirx=0;
- }
+  player.element.style.transform = `translate(${x}px, ${y}px)`;
 }
 
-document.addEventListener("keydown", teclaDown);
-document.addEventListener("keyup", teclaUp);
+function teclaDown(event) {
+  if (event.key === 'a') {
+    player.coordinates.x -= 10;
+  }
 
-function move(){
-      console.log();
-		pjy+=diry*vel;
-		pjx+=dirx*vel;
-		jog.style.top=pjy+"px";
-    	jog.style.left=pjx+"px";
+  if (event.key === 'd') {
+    player.coordinates.x += 10;
+  }
+
+  if (event.key === 's') {
+    player.coordinates.y += 10;
+  }
+
+  if (event.key === 'w') {
+    player.coordinates.y -= 10;
+  }
+
+  movePlayer(player);
 }
 
-function game(){
-      console.log();
-		if(jogo=true){
-		move();
- }
- var frames = requestAnimationFrame(game);
+function teclaUp(event) {
+  // ...
 }
 
-function inicia(){
- var jogo=true;
- tamTelaH=window.innerHeight;
- tamTelaW=window.innerWidth;
+document.addEventListener('keydown', teclaDown);
+document.addEventListener('keyup', teclaUp);
 
- dirx=0;
- diry=0;
- pjx=tamTelaW/2;
- pjy=tamTelaH/2;
- vel=5;
- jog=document.getElementById('aramisJog').appendChild(img);
- jog.style.top=pjy+"px";
- jog.style.left=pjx+"px";
- game();
-}
-}
